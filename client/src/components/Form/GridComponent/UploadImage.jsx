954151ee5338c79314421ea215ed430f870618e0
Ibrahim seh <ibrahim-seh@outlook.com>
2023-08-17 18:48:05 +0300

    QuiltedImageList

--- client/src/components/Form/GridComponent/UploadImage.jsx
+++ client/src/components/Form/GridComponent/UploadImage.jsx
@@ -1,54 +1,122 @@
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import { Container } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
+import QuiltedImageList from "./QuiltedImageList";
 
 const UploadImage = () => {
-  const [imageUrl, setImageUrl] = useState(null);
-  const [imageUrl1, setImageUrl1] = useState(null);
-  const [imageUrl2, setImageUrl2] = useState(null);
+  const [itemData, setItemData] = useState([]);
+  let tempItemData = [
+    {
+      img: "",
+      title: "",
+      rows: 2,
+      cols: 2,
+    },
+    {
+      img: "",
+      title: "",
+      rows: 3,
+      cols: 2,
+    },
+    {
+      img: "",
+      title: "",
+      rows: 2,
+      cols: 2,
+    },
+  ];
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
+    if (file) tempItemData[0].title = file.name;
     const file1 = event.target.files[1];
+    if (file1) tempItemData[1].title = file1.name;
     const file2 = event.target.files[2];
+    if (file2) tempItemData[2].title = file2.name;
     const reader = new FileReader();
     const reader1 = new FileReader();
     const reader2 = new FileReader();
 
     reader.onloadend = () => {
-      setImageUrl(reader.result);
+      tempItemData[0].img = reader.result;
+      setItemData(tempItemData);
     };
+
     reader1.onloadend = () => {
-      setImageUrl1(reader1.result);
+      tempItemData[1].img = reader1.result;
+      setItemData(tempItemData);
     };
+
     reader2.onloadend = () => {
-      setImageUrl2(reader2.result);
+      tempItemData[2].img = reader2.result;
+      setItemData(tempItemData);
     };
     if (file !== undefined) reader.readAsDataURL(file);
     if (file1 !== undefined) reader1.readAsDataURL(file1);
     if (file2 !== undefined) reader2.readAsDataURL(file2);
   };
 
+  const handelClick = (event) => {
+    event.stopPropagation();
+    console.log("yes");
+  };
+
   return (
     <Container maxWidth="md" sx={{ mt: 8 }}>
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <label htmlFor="upload-image">
-          {imageUrl && <img src={imageUrl} alt="Uploaded hj" height="300" />}
-          {imageUrl1 && <img src={imageUrl1} alt="Uploaded hj" height="300" />}
-          {imageUrl2 && <img src={imageUrl2} alt="Uploaded hj" height="300" />}
-          <Button variant="contained" component="span">
-            Add up to three photos
-          </Button>
-          <input
-            id="upload-image"
-            hidden
-            accept="image/*"
-            multiple
-            type="file"
-            onChange={handleFileUpload}
-          />
-        </label>
-      </Stack>
+      <label htmlFor="upload-image">
+        {itemData.length === 0 ? "" : <QuiltedImageList itemData={itemData} />}
+        <Button variant="contained" component="span">
+          Add up to three photos
+        </Button>
+        {itemData.length === 0 ? (
+          ""
+        ) : (
+          <Box
+            mt={-5}
+            //margin
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="flex-end"
+          >
+            <Button variant="outlined" color="secondary" onClick={handelClick}>
+              clear photos
+            </Button>
+          </Box>
+        )}
+
+        {/* <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Button
+                fullWidth
+                variant="outlined"
+                component="span"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Add up to three photos
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button
+                fullWidth
+                variant="outlined"
+                color="secondary"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handelClick}
+              >
+                clear photos
+              </Button>
+            </Grid>
+          </Grid> */}
+        <input
+          id="upload-image"
+          hidden
+          accept="image/*"
+          multiple
+          type="file"
+          onChange={handleFileUpload}
+        />
+      </label>
     </Container>
   );
 };
